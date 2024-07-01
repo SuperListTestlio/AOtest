aed014804e536fbfb4a6af6a41ffff3c190d74f3
Coltpvner <101257937+Coltpvner@users.noreply.github.com>
2024-07-01 18:27:05 +0300

    Add files via upload

--- test.php
+++ test.php
@@ -1,47 +1,8 @@
-<?php
-/**
- * OPML Parser
- *
- * @author Clint Ecker <phaedo@phaedo.cx>
- * @copyright Copyright (c) 2003 The MIT License
- * @version 1.0.2
- * @package External
- * @subpackage OPML Parser
- */
-
-class OPML {
-	var $file;
-	var $feeds;
-	var $template_file;
-	var $_links = array();
-
-	function OPML($file) {
-		$this->file   = $file;
-	}
-
-	function parseOPMLtoArray() {
-		$parser = xml_parser_create();
-		$data = implode('', file($this->file));
-		xml_parse_into_struct($parser,$data,$d_ar,$i_ar);
-
-		foreach($d_ar as $element) {
-			if($element['tag'] == 'OUTLINE') {
-				$feeds[] = $element['attributes'];
-			} 
-		}
-		$this->feeds = $feeds;
-	}
-
-	function load() {
-<<<<<<< Updated upstream
-		echo "<a href=\"$path_to_opml/". basename($this->file) . "\">Download OPML File</a>";
-=======
-		echo "<a href=\"$path_to_opml/". basename($this->file) . "\">Download OPML File2</a>";
->>>>>>> Stashed changes
-	}
-}
-?>
-chngs made
-edit
-changes
-CHANGES 1214
+<html>
+ <head>
+  <title>PHP Change</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
